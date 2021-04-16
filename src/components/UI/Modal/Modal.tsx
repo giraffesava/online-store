76d7b9fc0902c25a2bccfeb5b80557a1fe8e4207
Savelii <savely54@inbox.ru>
2021-04-16 23:41:26 +0700

    styling Modal

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
@@ -14,24 +14,61 @@ interface Props {
 
 const Modal:React.FC<Props> = ({
   modalhandler, overall, standardTickets, premiumTickets, vipTickets,
-}) => ReactDOM.createPortal((
-  <>
-    <Backdrop modalhandler={modalhandler} />
-    <div className={classes.container}>
-      <div className={classes.ticketsMenu}>
-        <h1>Your order:</h1>
-        <ul>
-          {!!standardTickets && <li>Standard:{standardTickets}</li>}
-          {!!premiumTickets && <li>Premium:{premiumTickets}</li>}
-          {!!vipTickets && <li>Vip:{vipTickets}</li>}
-        </ul>
-        <h1>
-          Overall:
-          {` ${overall}$`}
-        </h1>
+}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [promo, setPromo] = useState('');
+
+  const nameHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const emailHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const promoHandler = (e:React.ChangeEvent<HTMLInputElement>):number => {
+    setPromo(e.target.value);
+    return promo === 'hello' ? overall * 0.90 : overall;
+  };
+
+  return (ReactDOM.createPortal((
+    <>
+      <Backdrop modalhandler={modalhandler} />
+      <div className={classes.container}>
+        <div className={classes.ticketsMenu}>
+          <h1>Your order:</h1>
+          <ul>
+            {!!standardTickets && (
+            <li>Standard: {standardTickets}
+              {standardTickets === 1 ? ' ticket' : ' tickets'}
+            </li>
+            )}
+            {!!premiumTickets && (
+            <li>Premium: {premiumTickets}
+              {premiumTickets === 1 ? ' ticket' : ' tickets'}
+            </li>
+            )}
+            {!!vipTickets && (
+            <li>Vip: {vipTickets}
+              {vipTickets === 1 ? ' ticket' : ' tickets'}
+            </li>
+            )}
+          </ul>
+          <form>
+            <h3>Your name: </h3><input placeholder="Your name" value={name} type="text" onChange={nameHandler} />
+            <h3>Your email: </h3><input placeholder="Your email" value={email} type="email" onChange={emailHandler} />
+            <h3>Promo: </h3><input placeholder="Promo" value={promo} type="text" onChange={promoHandler} />
+            <button disabled type="submit">Submit</button>
+          </form>
+          <h1>
+            Overall:
+            {` ${promo === 'Sava' ? overall * 0.90 : overall}$`}
+          </h1>
+        </div>
       </div>
-    </div>
-  </>
-), document.getElementById('portal')!);
+    </>
+  ), document.getElementById('portal')!));
+};
 
 export default Modal;
