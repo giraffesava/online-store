5fcfb61b24c85ff2715c3a95367f94191f8d967f
Savelii <savely54@inbox.ru>
2021-04-17 21:39:36 +0700

    fixed some bugs

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,28 +1,36 @@
+/* eslint-disable max-len */
+/* eslint-disable react/button-has-type */
 import React, { FC, ReactNode } from 'react';
+import classNames from 'classnames/bind';
 import classes from './Button.module.css';
 import { AD_TYPES } from '../../Ad/ads';
 
 interface Props {
   children: ReactNode
-  buttonType: 'increment' | 'decrement',
-  onClick?: () => void,
+  buttonType: 'increment' | 'decrement' | 'submit',
+  onClick?: (arg:any) => void,
   check?: boolean,
-  title?: AD_TYPES
+  title?: AD_TYPES,
+  type?: 'button' | 'submit'
 }
+const allClasses = classNames.bind(classes);
 
 const Button: FC<Props> = ({
-  buttonType, onClick, title, check, children,
-}) => (
-  <>
-    <button
-      type="button"
-      className={[classes.increment, buttonType === 'decrement' ? classes.decrement : null].join(' ')}
-      onClick={onClick}
-      disabled={buttonType === 'decrement' && check}
-    >
-      {children}
-    </button>
-  </>
-);
+  buttonType, onClick, title, check, children, type,
+}) => {
+  const className = allClasses(buttonType);
+  return (
+    <>
+      <button
+        type={type}
+        className={className}
+        onClick={onClick}
+        disabled={check}
+      >
+        {children}
+      </button>
+    </>
+  );
+};
 
 export default Button;
--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,8 +1,19 @@
+/* eslint-disable max-len */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import ReactTooltip from 'react-tooltip';
+import { useDispatch } from 'react-redux';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
+import Button from '../Button/Button';
+import tooltip from './tooltip.svg';
+import {
+  resetTicketsStandard,
+  resetTicketsPremium,
+  resetTicketsVip,
+} from '../../../store/actions/index';
+import OrderMenu from '../OrderMenu/OrderMenu';
 
 interface Props {
   modalhandler: () => void
@@ -19,6 +30,8 @@ const Modal:React.FC<Props> = ({
   const [email, setEmail] = useState('');
   const [promo, setPromo] = useState('');
 
+  const dispatch = useDispatch();
+
   const nameHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   };
@@ -27,9 +40,15 @@ const Modal:React.FC<Props> = ({
     setEmail(e.target.value);
   };
 
-  const promoHandler = (e:React.ChangeEvent<HTMLInputElement>):number => {
+  const promoHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
     setPromo(e.target.value);
-    return promo === 'hello' ? overall * 0.90 : overall;
+  };
+
+  const submitHandler = (e:React.ChangeEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    dispatch(resetTicketsStandard());
+    dispatch(resetTicketsPremium());
+    dispatch(resetTicketsVip());
   };
 
   return (ReactDOM.createPortal((
@@ -38,33 +57,24 @@ const Modal:React.FC<Props> = ({
       <div className={classes.container}>
         <div className={classes.ticketsMenu}>
           <h1>Your order:</h1>
-          <ul>
-            {!!standardTickets && (
-            <li>Standard: {standardTickets}
-              {standardTickets === 1 ? ' ticket' : ' tickets'}
-            </li>
-            )}
-            {!!premiumTickets && (
-            <li>Premium: {premiumTickets}
-              {premiumTickets === 1 ? ' ticket' : ' tickets'}
-            </li>
-            )}
-            {!!vipTickets && (
-            <li>Vip: {vipTickets}
-              {vipTickets === 1 ? ' ticket' : ' tickets'}
-            </li>
-            )}
-          </ul>
-          <form>
-            <h3>Your name: </h3><input placeholder="Your name" value={name} type="text" onChange={nameHandler} />
-            <h3>Your email: </h3><input placeholder="Your email" value={email} type="email" onChange={emailHandler} />
-            <h3>Promo: </h3><input placeholder="Promo" value={promo} type="text" onChange={promoHandler} />
-            <button disabled type="submit">Submit</button>
-          </form>
-          <h1>
+          <div className={classes.order}>
+            <OrderMenu ticketType={standardTickets} price={250}>Standard</OrderMenu>
+            <OrderMenu ticketType={premiumTickets} price={450}>Standard</OrderMenu>
+            <OrderMenu ticketType={vipTickets} price={850}>Standard</OrderMenu>
+          </div>
+          <h2 className={classes.overall}>
             Overall:
-            {` ${promo === 'Sava' ? overall * 0.90 : overall}$`}
-          </h1>
+            {` ${promo === 'Sava' ? overall * 0.85 : overall}$`}
+          </h2>
+          <div className={classes.formContainer}>
+            <form>
+              <h3>Your name: </h3><input placeholder="Name" value={name} type="text" minLength={4} onChange={nameHandler} />
+              <h3>Your email: </h3><input placeholder="Email" value={email} type="email" onChange={emailHandler} />
+              <h3>Promo<div className={classes.tooltip}><img src={tooltip} alt="vav" data-tip="The name of the creator of this site(look header)" /><ReactTooltip font-size="50px" /></div>:
+              </h3><input placeholder="Promo" value={promo} type="text" onChange={promoHandler} />
+              <Button onClick={submitHandler} check={!(name.length >= 4) || !(email.length >= 4)} buttonType="submit">Submit</Button>
+            </form>
+          </div>
         </div>
       </div>
     </>
