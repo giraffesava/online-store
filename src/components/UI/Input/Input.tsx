b5b515863132c310af8f0c76f86e1843c1bae751
Savelii <savely54@inbox.ru>
2021-04-18 17:46:06 +0700

    splitted on components

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,25 @@
+import React, { forwardRef } from 'react';
+
+interface Props {
+  placeholder: string
+  value: string
+  type: string
+  minLength: number
+  onChange: (arg: React.ChangeEvent<HTMLInputElement>) => void
+  ref: React.RefObject<HTMLInputElement>
+}
+
+const Input = forwardRef<HTMLInputElement, Props>(({
+  placeholder, value, type, minLength, onChange,
+}, ref) => (
+  <input
+    placeholder={placeholder}
+    value={value}
+    type={type}
+    minLength={minLength}
+    onChange={onChange}
+    ref={ref}
+  />
+));
+
+export default Input;
--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,19 +1,9 @@
-/* eslint-disable max-len */
-/* eslint-disable react/jsx-one-expression-per-line */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactTooltip from 'react-tooltip';
-import { useDispatch } from 'react-redux';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
-import Button from '../Button/Button';
-import tooltip from './tooltip.svg';
-import {
-  resetTicketsStandard,
-  resetTicketsPremium,
-  resetTicketsVip,
-} from '../../../store/actions/index';
 import OrderMenu from '../OrderMenu/OrderMenu';
+import Form from '../../Form/Form';
 
 interface Props {
   modalhandler: () => void
@@ -25,60 +15,25 @@ interface Props {
 
 const Modal:React.FC<Props> = ({
   modalhandler, overall, standardTickets, premiumTickets, vipTickets,
-}) => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [promo, setPromo] = useState('');
-
-  const dispatch = useDispatch();
-
-  const nameHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-
-  const emailHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const promoHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setPromo(e.target.value);
-  };
-
-  const submitHandler = (e:React.ChangeEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    dispatch(resetTicketsStandard());
-    dispatch(resetTicketsPremium());
-    dispatch(resetTicketsVip());
-  };
-
-  return (ReactDOM.createPortal((
-    <>
-      <Backdrop modalhandler={modalhandler} />
-      <div className={classes.container}>
-        <div className={classes.ticketsMenu}>
-          <h1>Your order:</h1>
+}) => (ReactDOM.createPortal((
+  <>
+    <Backdrop modalhandler={modalhandler} />
+    <div className={classes.container}>
+      <div className={classes.ticketsMenu}>
+        <h1>Your order:</h1>
+        {overall ? (
           <div className={classes.order}>
             <OrderMenu ticketType={standardTickets} price={250}>Standard</OrderMenu>
             <OrderMenu ticketType={premiumTickets} price={450}>Standard</OrderMenu>
             <OrderMenu ticketType={vipTickets} price={850}>Standard</OrderMenu>
           </div>
-          <h2 className={classes.overall}>
-            Overall:
-            {` ${promo === 'Sava' ? overall * 0.85 : overall}$`}
-          </h2>
-          <div className={classes.formContainer}>
-            <form>
-              <h3>Your name: </h3><input placeholder="Name" value={name} type="text" minLength={4} onChange={nameHandler} />
-              <h3>Your email: </h3><input placeholder="Email" value={email} type="email" onChange={emailHandler} />
-              <h3>Promo<div className={classes.tooltip}><img src={tooltip} alt="vav" data-tip="The name of the creator of this site(look header)" /><ReactTooltip font-size="50px" /></div>:
-              </h3><input placeholder="Promo" value={promo} type="text" onChange={promoHandler} />
-              <Button onClick={submitHandler} check={!(name.length >= 4) || !(email.length >= 4)} buttonType="submit">Submit</Button>
-            </form>
-          </div>
+        ) : <p className={classes.chooseTitle}>Please choose any program</p>}
+        <div className={classes.formContainer}>
+          <Form overall={overall} />
         </div>
       </div>
-    </>
-  ), document.getElementById('portal')!));
-};
+    </div>
+  </>
+), document.getElementById('portal')!));
 
 export default Modal;
